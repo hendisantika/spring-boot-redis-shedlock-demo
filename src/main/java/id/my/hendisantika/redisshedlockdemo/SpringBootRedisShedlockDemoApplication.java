5015dbdb347483a821e972261d04903639b56c72
hendisantika <hendisantika@yahoo.co.id>
2024-12-30 18:04:27 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/redisshedlockdemo/SpringBootRedisShedlockDemoApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.redisshedlockdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisShedlockDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisShedlockDemoApplication.class, args);
+    }
+
+}
